2ce7b8e5c05cc086040ee975f4884dd8f87fdffa
Farouk Ben fraj <farouk.ben-fraj@epitech.eu>
2019-08-18 16:22:35 +0200

    ShowBookList functionnal

--- client/src/components/ShowBookList.jsx
+++ client/src/components/ShowBookList.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import BookCard from "./BookCard";
 
 function ShowBookList() {
-    return(
-        <div>ShowBookList</div>
-    );
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8082/api/books")
+      .then(res => {
+        setBooks(res.data);
+      })
+      .catch(err => {
+        console.log("Error from ShowBookList");
+      });
+  });
+
+  console.log("PrintBook: " + books);
+  let bookList;
+  if (!books) {
+    bookList = "There is no book record!";
+  } else {
+    bookList = books.map((book, index) => <BookCard book={book} key={index} />);
+  }
+
+  return (
+    <div className="ShowBookList">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <br />
+            <h2 className="display-4 text-center">Books List</h2>
+          </div>
+
+          <div className="col-md-11">
+            <Link
+              to="/create-book"
+              className="btn btn-outline-warning float-right"
+            >
+              + Add New Book
+            </Link>
+            <br />
+            <br />
+            <hr />
+          </div>
+        </div>
+
+        <div className="list">{bookList}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ShowBookList;
